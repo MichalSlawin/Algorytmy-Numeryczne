7c468dfefb1feb4bdd61248ffc68e7f02aa41828
psiewruk <patryk.siewruk@gmail.com>
2018-11-12 15:59:43 +0100

    Usuniecie ArithmeticsIntefrace

--- src/main/java/ArithmeticsInterface.java
+++
@@ -1,9 +0,0 @@
-//package main.java;
-
-//testowe, interfejs jeszcze nie dziala - na razie castuje wszystko na double
-//nie usuwam tego z nadzieją, że uda się to później jednak wdrożyć aby uprościć kod
-interface ArithmeticsInterface<T extends Number> {
-    T zero(); // Adding zero items
-    T add(T lhs, T rhs);
-    T multiply(T lhs, T rhs);
-}
