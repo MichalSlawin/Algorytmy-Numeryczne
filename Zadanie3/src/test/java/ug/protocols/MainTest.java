bead34ac6f2f53acda5dbb2030d0a6e25bd5a62d
Michal Slawinski <michal.slawin@gmail.com>
2018-12-09 18:10:30 +0100

    dodano porownanie metod z MyMatrix z symulacja

--- Zadanie3/src/test/java/ug/protocols/MainTest.java
+++ Zadanie3/src/test/java/ug/protocols/MainTest.java
@@ -7,16 +7,17 @@ import static ug.protocols.agent.Simulations.simulateAllVotings;
 
 public class MainTest {
 
-	private static final int AGENTS_COUNT = 4;
+	private static final int AGENTS_COUNT = 10;
 	private static final double EPSILON = 1e-12;
 	private static final int SESSIONS = 10000;
 	
     public static void main(String [] args) {
+		double monteCarloNorm = simulateAllVotings(AGENTS_COUNT, SESSIONS).vectorNorm();
 //    	timesTest();
 
-//		System.out.println("jacobiNorm - seidelNorm = " + compareJacobiSeidel());
-//    	System.out.println("GaussPg - GaussPgOpt = " + compareGausses());
-    	System.out.println(simulateAllVotings(AGENTS_COUNT, SESSIONS));
+		System.out.println("jacobiNorm - seidelNorm = " + compareJacobiSeidel(monteCarloNorm));
+    	System.out.println("GaussPg - GaussPgOpt = " + compareGausses(monteCarloNorm));
+    	//System.out.println(simulateAllVotings(AGENTS_COUNT, SESSIONS));
     }
 
     private static void timesTest() {
@@ -45,32 +46,36 @@ public class MainTest {
 		System.out.println("Czas Jacobiego: " + executionTime);
     }
 
-	private static double compareJacobiSeidel() {
+	private static double compareJacobiSeidel(double monteCarloNorm) {
 		Equations e = new Equations(AGENTS_COUNT);
 
 		MyMatrix jacobiResult = e.getMatrix().jacobi(e.getVector(), EPSILON);
 		//System.out.println("Z jacobiego:\n" + jacobiResult.transpose());
 		double jacobiNorm = jacobiResult.vectorNorm();
+		System.out.println("|monteCarloNorm - jacobiNorm| = " + Math.abs(monteCarloNorm - jacobiNorm));
 
 		MyMatrix seidelResult = e.getMatrix().gaussSeidel(e.getVector(), EPSILON);
 		//System.out.println("Z seidela:\n" + seidelResult.transpose());
 		double seidelNorm = seidelResult.vectorNorm();
+		System.out.println("|monteCarloNorm - seidelNorm| = " + Math.abs(seidelNorm - jacobiNorm));
 
 		return jacobiNorm-seidelNorm;
 	}
 	
-	private static double compareGausses() {
+	private static double compareGausses(double monteCarloNorm) {
 		Equations e = new Equations(AGENTS_COUNT);
 
 		MyMatrix gaussPgResult = e.getMatrix().gaussPG(e.getVector());
 		//System.out.println("GaussPg:\n" + gaussPgResult.transpose());
-		double guassPgNorm = gaussPgResult.vectorNorm();
+		double gaussPgNorm = gaussPgResult.vectorNorm();
+		System.out.println("|monteCarloNorm - gaussPgNorm| = " + Math.abs(monteCarloNorm - gaussPgNorm));
 
 		MyMatrix gaussPgOptResult = e.getMatrix().gaussPGOpt(e.getVector());
 		//System.out.println("GaussPg zoptymalizowany:\n" + gaussPgOptResult.transpose());
-		double guassPgOptNorm  = gaussPgOptResult.vectorNorm();
+		double gaussPgOptNorm  = gaussPgOptResult.vectorNorm();
+		System.out.println("|monteCarloNorm - gaussPgOptNorm| = " + Math.abs(monteCarloNorm - gaussPgOptNorm));
 
-		return guassPgNorm-guassPgOptNorm;
+		return gaussPgNorm-gaussPgOptNorm;
 	}
 
 }
